7cd8e567f2579d8d5cefa49cfc444c365106fe32
jeffhoelter <jeffh@loar.net>
2015-09-17 14:55:04 -0400

    Tests passing through flush?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -4,25 +4,31 @@
   (let [[_ snd] card]
     (str snd)))
 
-(def replacements {\A 100, \B 20})
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
 
 (defn rank [card]
   (let [[fst _] card]
     (if (Character/isDigit fst)
       (Integer/valueOf (str fst))
-      (str fst))))
+      (replacements fst))))
+
+(defn frequency-checker [hand frequency]
+  (> (apply max (vals (frequencies (map rank hand)))) frequency))
 
 (defn pair? [hand]
-  nil)
+  (frequency-checker hand 1))
+
+(defn old-pair? [hand]
+  (> (apply max (vals (frequencies (map rank hand)))) 1))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (frequency-checker hand 2))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (frequency-checker hand 3))
 
 (defn flush? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map suit hand)))) 4))
 
 (defn full-house? [hand]
   nil)
